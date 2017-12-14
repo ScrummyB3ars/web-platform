21ed99148642dc3711cba5a23f6aa2881c9826db
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-14 18:11:45 +0100

    add tip overview and single tip routing

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -1,5 +1,61 @@
 import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import { Button } from 'material-ui';
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow
+} from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+import { Link } from 'react-router-dom';
 
-export default () => {
-  return <h1>Tips</h1>;
-};
+import TipDetail from './TipDetail';
+
+const styles = theme => ({
+  root: {
+    marginTop: theme.spacing.unit * 3
+  }
+});
+
+const data = [{ id: 0, content: 'Zever' }, { id: 1, content: 'Meer zever' }];
+
+function Tips(props) {
+  const { classes, onDelete, match: { params: { id } } } = props;
+
+  if (id) {
+    return <TipDetail />;
+  }
+
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell numeric>ID</TableCell>
+            <TableCell>Content</TableCell>
+            <TableCell />
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map(n => {
+            return (
+              <TableRow key={n.id}>
+                <TableCell numeric>{n.id}</TableCell>
+                <TableCell>{n.content}</TableCell>
+                <TableCell>
+                  <Button onClick={() => this.onDelete(n.id)}>Delete</Button>
+                  <Link to={`/tips/${n.id}`}>
+                    <Button>Detail</Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+export default withStyles(styles)(Tips);
