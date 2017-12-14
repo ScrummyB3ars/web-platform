2af7871db60c2ff194ef3260ec9482c672127893
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-14 12:03:18 +0100

    get rid of warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Redirect,
-  withRouter
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 
