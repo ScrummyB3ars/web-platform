9d2b1d7e7ddcb38f18f8b21e41885b999b276957
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-16 20:42:21 +0100

    add subscribe form

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { Button, Input, Card, FormControl } from 'material-ui';
 
 export default () => {
-  return <h1>Registreer</h1>;
+  return (
+    <div className="container-center-items">
+      <form>
+        <Card className="login-card">
+          <FormControl>
+            <Input
+              placeholder="Email"
+              inputProps={{
+                name: 'email',
+                onChange: e => this.handleInputChange(e)
+              }}
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            raised
+            color="primary"
+            onClick={e => this.onSubmit(e)}
+          >
+            Register
+          </Button>
+        </Card>
+      </form>
+    </div>
+  );
 };
