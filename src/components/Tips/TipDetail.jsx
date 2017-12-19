c4f5a978ab972f014dad1ba15eca928ba1061d2c
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-19 13:37:18 +0100

    load tips on fresh load

--- src/components/Tips/TipDetail.jsx
+++ src/components/Tips/TipDetail.jsx
@@ -1,14 +1,50 @@
 import React from 'react';
+import { CircularProgress } from 'material-ui/Progress';
 
 import TipThemeDetail from './TipThemeDetail';
 import TipInteractionDetail from './TipInteractionDetail';
 
-export default props => {
-  const { match: { params: { type } } } = props;
-  const { tip, themes } = props.location.state;
-  return type === 'theme' ? (
-    <TipThemeDetail tip={tip} themes={themes} />
-  ) : (
-    <TipInteractionDetail tip={tip} />
-  );
-};
+class TipDetail extends React.Component {
+  componentWillMount() {
+    if (!this.props.themeTips || !this.props.interactionTips) {
+      this.props.getAllTips();
+    }
+  }
+  render() {
+    const {
+      match: { params: { type, id } },
+      interactionTips,
+      themeTips,
+      themes
+    } = this.props;
+
+    if (!themeTips || !interactionTips) {
+      return (
+        <div style={{ height: '50vh' }} className="container-center-items">
+          <CircularProgress size={50} />
+        </div>
+      );
+    }
+
+    if (id === 'new') {
+      return type === 'theme' ? (
+        <TipThemeDetail addNew themes={themes} />
+      ) : (
+        <TipInteractionDetail addNew />
+      );
+    } else {
+      return type === 'theme' ? (
+        <TipThemeDetail
+          tip={themeTips.filter(t => t.id === parseInt(id))[0]}
+          themes={themes}
+        />
+      ) : (
+        <TipInteractionDetail
+          tip={interactionTips.filter(t => t.id === parseInt(id))[0]}
+        />
+      );
+    }
+  }
+}
+
+export default TipDetail;
