3afca6993792a6d29f1eae6bc702375a68bb6396
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-12-19 14:48:03 +0100

    add validation

--- src/components/Tips/TipThemeDetail.jsx
+++ src/components/Tips/TipThemeDetail.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
-import TextField from 'material-ui/TextField';
-import Input from 'material-ui/Input';
-import Select from 'material-ui/Select';
 import { MenuItem } from 'material-ui/Menu';
 import Grid from 'material-ui/Grid';
-import { FormControl, InputLabel } from 'material-ui';
+import { Button, FormControl } from 'material-ui';
+import { ValidatorForm } from 'react-form-validator-core';
+import {
+  TextValidator,
+  SelectValidator
+} from 'react-material-ui-form-validator';
 
 const styles = theme => ({
   select: {
@@ -14,70 +16,153 @@ const styles = theme => ({
   },
   textfield: {
     margin: theme.spacing.unit
+  },
+  submit: {
+    marginTop: '2em',
+    display: 'block',
+    width: '100%'
   }
 });
 
-const Detail = props => {
-  const { tip, themes, classes } = props;
-  return (
-    <Grid container>
-      <FormControl fullWidth="true" className={classes.textfield}>
-        <TextField
-          id="multiline-flexible"
-          label="Content"
-          fullWidth
-          multiline
-          value={tip.tip_content}
-          className="tip-content"
-          margin="normal"
-        />
-      </FormControl>
-      <FormControl fullWidth="true" className={classes.textfield}>
-        <TextField
-          label="Development goal"
-          fullWidth
-          multiline
-          value={tip.development_goal}
-          margin="normal"
-        />
-      </FormControl>
-      <FormControl fullWidth="true" className={classes.textfield}>
-        <TextField
-          label="ZILL goal"
-          fullWidth
-          multiline
-          value={tip.zill_goal}
-          margin="normal"
-        />
-      </FormControl>
-      <FormControl className={classes.select}>
-        <InputLabel htmlFor="rich-language">Rich language</InputLabel>
-        <Select
-          value={tip.rich_language}
-          input={<Input name="rich-language" />}
-        >
-          <MenuItem value="voorspellen">Voorspellen</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.select}>
-        <InputLabel htmlFor="circumstances">Circumstances</InputLabel>
-        <Select
-          value={tip.circumstances}
-          input={<Input name="circumstances" />}
-        >
-          <MenuItem value="wolken, kans op neerslag">
-            Wolken, kans op neerslag
-          </MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl className={classes.select}>
-        <InputLabel htmlFor="theme">Theme</InputLabel>
-        <Select value={tip.themes_id} input={<Input name="theme" />}>
-          {themes.map(th => <MenuItem value={th.id}>{th.name}</MenuItem>)}
-        </Select>
-      </FormControl>
-    </Grid>
-  );
-};
+class Detail extends React.Component {
+  constructor(props) {
+    super(props);
+    if (this.props.addNew) {
+      this.state = {
+        tip: {
+          tip_content: '',
+          development_goal: '',
+          zill_goal: '',
+          rich_language: -1,
+          circumstances: -1,
+          themes_id: -1
+        }
+      };
+    } else {
+      this.state = { tip: props.tip };
+    }
 
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      tip: { ...this.state.tip, [name]: value }
+    });
+  }
+
+  handleSubmit() {}
+
+  render() {
+    const { themes, classes } = this.props;
+    const { tip } = this.state;
+    return (
+      <ValidatorForm
+        ref="form"
+        onSubmit={this.handleSubmit}
+        onError={errors => console.log(errors)}
+      >
+        <Grid container>
+          <FormControl
+            required="true"
+            fullWidth="true"
+            className={classes.textfield}
+          >
+            <TextValidator
+              label="Content"
+              fullWidth
+              multiline
+              name="tip_content"
+              value={tip.tip_content}
+              className="tip-content"
+              margin="normal"
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl fullWidth="true" className={classes.textfield}>
+            <TextValidator
+              label="Development goal"
+              name="development_goal"
+              fullWidth
+              multiline
+              value={tip.development_goal}
+              margin="normal"
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl fullWidth="true" className={classes.textfield}>
+            <TextValidator
+              label="ZILL goal"
+              name="zill_goal"
+              fullWidth
+              multiline
+              value={tip.zill_goal}
+              margin="normal"
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl className={classes.select}>
+            <SelectValidator
+              label="Rich language"
+              value={tip.rich_language}
+              name="rich_language"
+              children={() => {
+                return <MenuItem>voorspellen</MenuItem>;
+              }}
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl className={classes.select}>
+            <SelectValidator
+              label="Circumstances"
+              value={tip.circumstances}
+              name="circumstances"
+              children={() => {
+                return <MenuItem>wolken</MenuItem>;
+              }}
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl className={classes.select}>
+            <SelectValidator
+              label="Theme"
+              name="themes_id"
+              value={tip.themes_id}
+              children={themes.map(th => {
+                return (
+                  <MenuItem key={th.id} value={th.id}>
+                    {th.name}
+                  </MenuItem>
+                );
+              })}
+              onChange={this.handleInputChange}
+              validators={['required']}
+              errorMessages={['this field is required']}
+            />
+          </FormControl>
+          <FormControl className={classes.submit}>
+            <Button raised color="primary" type="submit">
+              Edit
+            </Button>
+          </FormControl>
+        </Grid>
+      </ValidatorForm>
+    );
+  }
+}
 export default withStyles(styles)(Detail);
